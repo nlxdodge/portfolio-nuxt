046670de12bc2a553f089ea1bbc66c07478653a8
NLxDoDge <nlxdodge@gmail.com>
2022-12-24 23:33:46 +0100

    Fix titles and fix deployment script

--- pages/posts/[...slugs].vue
+++ pages/posts/[...slugs].vue
@@ -1,3 +1,7 @@
 <template>
-    <ContentDoc />
-</template>
+    <ContentDoc v-slot="{ doc }">
+      <h1>{{ doc.title }}</h1>
+      <ContentRenderer :value="doc" />
+    </ContentDoc>
+  </template>
+  
